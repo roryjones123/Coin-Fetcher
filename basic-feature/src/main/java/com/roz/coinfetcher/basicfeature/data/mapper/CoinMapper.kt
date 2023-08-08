1b7108643aa204b045367645548888f5bbe7a24e
Rory Jones <rory.jones@PBXUK-HH-05931.photobox.priv>
2023-08-08 11:20:45 +0100

    mapper update for complex domain

--- basic-feature/src/main/java/com/roz/coinfetcher/basicfeature/data/mapper/CoinMapper.kt
+++ basic-feature/src/main/java/com/roz/coinfetcher/basicfeature/data/mapper/CoinMapper.kt
@@ -2,6 +2,8 @@ package com.roz.coinfetcher.basicfeature.data.mapper
 
 import com.roz.coinfetcher.basicfeature.data.remote.model.CoinResponse
 import com.roz.coinfetcher.basicfeature.domain.model.Coin
+import com.roz.coinfetcher.basicfeature.domain.model.ComplexCoin
+import com.roz.coinfetcher.basicfeature.domain.model.Tag
 
 fun CoinResponse.toDomainModel() = Coin(
     id = id,
@@ -11,4 +13,36 @@ fun CoinResponse.toDomainModel() = Coin(
     isNew = isNew,
     isActive = isActive,
     type = type
+)
+
+
+fun CoinResponse.toComplexDomainModel() = ComplexCoin(
+    id = id,
+    name = name,
+    symbol = symbol,
+    parent = parent,
+    rank = rank,
+    isNew = isNew,
+    isActive = isActive,
+    type = type,
+    logo = logo,
+    tags = tags,
+    team = team,
+    description = description,
+    message = message,
+    openSource = openSource,
+    hardwareWallet = hardwareWallet,
+    startedAt = startedAt,
+    developmentStatus = developmentStatus,
+    proofType = proofType,
+    orgStructure = orgStructure,
+    hashAlgorithm = hashAlgorithm,
+    contract = contract,
+    platform = platform,
+    contracts = contracts,
+    links = links,
+    linksExtended = linksExtended,
+    whitepaper = whitepaper,
+    firstDataAt = firstDataAt,
+    lastDataAt = lastDataAt
 )
