9392fb97756490cfe534bb83eb2e4f463ba0e7bd
Rory Jones <rory.jones@PBXUK-HH-05931.photobox.priv>
2023-08-09 08:03:09 +0100

    extract filter logic to own object
    filtering of tag logic

---
+++ basic-feature/src/main/java/com/roz/coinfetcher/basicfeature/presentation/utils/ApplyFilterUtil.kt
@@ -0,0 +1,46 @@
+package com.roz.coinfetcher.basicfeature.presentation.utils
+
+import com.roz.coinfetcher.basicfeature.presentation.HomepageUiState
+import com.roz.coinfetcher.basicfeature.presentation.model.CoinDisplayable
+import com.roz.coinfetcher.basicfeature.presentation.model.TagDisplayable
+
+// runs through the filtering process, filtering coins based on selected tags
+object ApplyFilterUtil {
+    fun applyFilters(
+        previousState: HomepageUiState,
+        partialState: HomepageUiState.PartialState.Filter
+    ): HomepageUiState {
+        val tags = mapTags(previousState.tags, partialState.tagPressed)
+        val selectedTags = filterSelectedTags(tags)
+        val coins = mapCoins(previousState.coins, selectedTags)
+
+        return previousState.copy(tags = tags, coins = coins)
+    }
+
+    private fun mapTags(tags: List<TagDisplayable>, tagPressed: TagDisplayable): List<TagDisplayable> {
+        return tags.map { tag ->
+            if (tag == tagPressed) {
+                tag.copy(isSelected = !tag.isSelected)
+            } else {
+                tag
+            }
+        }.sortedByDescending { it.isSelected }
+    }
+
+    private fun filterSelectedTags(tags: List<TagDisplayable>): List<TagDisplayable> {
+        return tags.filter { it.isSelected }
+    }
+
+    private fun mapCoins(coins: List<CoinDisplayable>, selectedTags: List<TagDisplayable>): List<CoinDisplayable> {
+        return coins.map { coin ->
+            val coinMatchesFilters = if (selectedTags.isEmpty()) {
+                true
+            } else {
+                selectedTags.all { selectedTag ->
+                    selectedTag.taggedItems?.contains(coin.id) == true
+                }
+            }
+            coin.copy(isVisible = coinMatchesFilters)
+        }
+    }
+}
